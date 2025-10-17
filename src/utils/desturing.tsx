a4ba3523b8781a01cd97e9cec9c8669c2cbb473a
ardinsyah-reva <revaardiansyahabdullah@gmail.com>
2025-10-17 20:29:37 +0700

    first commit

---
+++ src/utils/desturing.tsx
@@ -0,0 +1,16 @@
+
+// object
+export const object = `
+const product = { nameProduct: 'kulkas',price: 1000000 };
+`;
+
+export const product = { nameProduct: 'kulkas',price: 1000000 };
+export const { nameProduct, price } = product
+
+// array
+export const array = `
+const mobil = ['toyota', 'civic', 'lancer', 'mazda']
+`;
+
+export const mobil = ['toyota', 'civic', 'lancer', 'mazda'];
+export const [first, second, third] = mobil
---
+++ src/utils/logic.tsx
@@ -0,0 +1,4 @@
+export const arrowFunction = `
+const sum = (a: number,b: number) => a + b ;`;
+
+export const sum = (a: number,b: number) => a + b ;
